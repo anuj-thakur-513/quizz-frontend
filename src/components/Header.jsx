b231b69845c1b0cc0ef992146fc41b2829ed6953
anuj-thakur-513 <anujthakur2003.anuj@gmail.com>
2025-01-09 23:33:33 +0530

    feat: add header

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+const Header = () => {
+  return (
+    <header className="bg-blue-600 text-white shadow-md">
+      <div className="container px-4 py-3">
+        <p className="text-2xl font-bold">Quizz</p>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
